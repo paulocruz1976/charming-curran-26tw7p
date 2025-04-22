c00609cf58bf32b14bf1ecf1bc37aee29c73eca0
Paulo Cruz <gigante700@gmail.com>
2025-04-22 19:23:14 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Criação de projetos com CodeSandbox</h1>
+      <h2>LUIZA HELENA MEU ETERNO AMOR</h2>
+    </div>
+  );
+}
